e1dc12cb1f48cd763672f3aec0cdb081f6e0ecbe
hanwy <yyydyhanwy@163.com>
2015-06-10 13:21:32 +0800

    更改refresh刷新文字

--- Classes/Other/Lib/MJRefresh/MJRefreshConst.m
+++ Classes/Other/Lib/MJRefresh/MJRefreshConst.m
@@ -12,10 +12,10 @@ NSString *const MJRefreshContentOffset = @"contentOffset";
 NSString *const MJRefreshContentSize = @"contentSize";
 NSString *const MJRefreshPanState = @"pan.state";
 
-NSString *const MJRefreshHeaderStateIdleText = @"下拉可以刷新";
-NSString *const MJRefreshHeaderStatePullingText = @"松开立即刷新";
-NSString *const MJRefreshHeaderStateRefreshingText = @"正在刷新数据中...";
+NSString *const MJRefreshHeaderStateIdleText = @"下拉刷新";
+NSString *const MJRefreshHeaderStatePullingText = @"释放刷新";
+NSString *const MJRefreshHeaderStateRefreshingText = @"刷新数据中...";
 
 NSString *const MJRefreshFooterStateIdleText = @"点击加载更多";
-NSString *const MJRefreshFooterStateRefreshingText = @"正在加载更多的数据...";
-NSString *const MJRefreshFooterStateNoMoreDataText = @"已经全部加载完毕";
+NSString *const MJRefreshFooterStateRefreshingText = @"加载数据中...";
+NSString *const MJRefreshFooterStateNoMoreDataText = @"加载完毕";
